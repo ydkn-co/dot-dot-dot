faf9e33ffe2cae94256ec43086c5642ee7b3d485
Wes Rice <wes@wesrice.com>
2021-11-11 16:51:22 -0500

    Integrate scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,57 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
 
-function App() {
+import styles from './App.module.scss'
+import logo from './logo.svg'
+
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div
+      className={styles.App}
+    >
+      <header
+        className={styles.AppHeader}
+      >
+        <img
+          alt="logo"
+          className={styles.AppLogo}
+          src={logo}
+        />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+          <button
+            data-testid="button"
+            onClick={() => setCount((count) => count + 1)}
+            type="button"
+          >
+            count is:
+            {' '}
+            {count}
           </button>
         </p>
         <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
+          Edit
+          {' '}
+          <code>App.tsx</code>
+          {' '}
+          and save to test HMR updates.
         </p>
         <p>
           <a
-            className="App-link"
+            className={styles.AppLink}
             href="https://reactjs.org"
-            target="_blank"
             rel="noopener noreferrer"
+            target="_blank"
           >
             Learn React
           </a>
           {' | '}
           <a
-            className="App-link"
+            className={styles.AppLink}
             href="https://vitejs.dev/guide/features.html"
-            target="_blank"
             rel="noopener noreferrer"
+            target="_blank"
           >
             Vite Docs
           </a>
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
+import 'normalize.css'
+import './index.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+
 import App from './App'
 
 ReactDOM.render(
