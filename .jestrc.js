311174d1cf399d63c159b840481e7cef534f6f3e
Wes Rice <wes@wesrice.com>
2021-11-11 17:11:21 -0500

    Replace `ts-jest` with `@swc/jest`

--- .jestrc.js
+++ .jestrc.js
@@ -4,6 +4,19 @@ module.exports = {
     // eslint-disable-next-line max-len
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js'
   },
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          transform: {
+            react: {
+              runtime: 'automatic'
+            }
+          }
+        }
+      }
+    ]
+  }
 }
