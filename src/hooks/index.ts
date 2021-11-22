a783fb38e3039dec21f0038d4e122ee9f424886f
Wes Rice <wes@wesrice.com>
2021-11-22 07:41:37 -0500

    Remove unneeded files

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,3 @@
 export { default as useControlledInterval } from './useControlledInterval'
-export { default as usePresence } from './usePresence'
 export { default as useRandomExpressiveColor } from './useRandomExpressiveColor'
 export { default as useDebounce } from './useDebounce'
