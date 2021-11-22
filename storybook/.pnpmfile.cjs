7b27d662db4a859d304ec85005d2a9c18aed8898
Wes Rice <wes@wesrice.com>
2021-11-21 21:57:50 -0500

    Remove storybook

--- storybook/.pnpmfile.cjs
+++
@@ -1,9 +0,0 @@
-function readPackage(packageJson, context) {
-  // Workaround for https://github.com/storybookjs/storybook/issues/16705
-  if (packageJson.name === "@storybook/addon-docs") {
-    context.log("Fixing the acorn dependency for @storybook/addon-docs");
-    packageJson.dependencies.acorn = "^8.5.0";
-  }
-
-  return packageJson;
-}
